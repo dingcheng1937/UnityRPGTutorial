ffcdf6e1a494de9a38750c5c2867b1402aa27844
dingcheng1937 <dcrlikercd@126.com>
2024-03-14 15:24:39 +0800

    create cinematics
    Make Cinematic Sequence
    Trigger Cinemachine Cut Scene
    The Observer Pattern In C#
    Player Input In Cutscenes

---
+++ RPG Project/Assets/Scripts/Cinematrics/CinematicControlRemover.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Control;
+using RPG.Core;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicControlRemover : MonoBehaviour
+    {
+        GameObject player;
+        void Start()
+        {
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+            player = GameObject.FindWithTag("Player");
+        }
+
+        // Update is called once per frame
+        void DisableControl(PlayableDirector pd)
+        {
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
+        }
+
+        void EnableControl(PlayableDirector pd)
+        {
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+    }
+}
---
+++ RPG Project/Assets/Scripts/Cinematrics/CinematicTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private bool isTriggered = false;
+        private void OnTriggerEnter(Collider other) {
+            if (!isTriggered && other.gameObject.tag == "Player")
+            {
+                GetComponent<PlayableDirector>().Play();
+                isTriggered = true;
+            }
+            
+        }
+    }
+}
