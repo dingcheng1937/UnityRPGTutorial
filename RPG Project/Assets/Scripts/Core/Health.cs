c55c7e146f2ecce38b7daeb62d8f15bb5c3a0f9f
dingcheng1937 <dcrlikercd@126.com>
2024-03-22 19:13:33 +0800

    [temp] saving system

--- RPG Project/Assets/Scripts/Core/Health.cs
+++ RPG Project/Assets/Scripts/Core/Health.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using RPG.Saving;
 using UnityEngine;
 
 namespace RPG.Core
 {
-    public class Health : MonoBehaviour
+    public class Health : MonoBehaviour, ISaveable
     {
         [SerializeField] float health = 100f;
         [SerializeField] float maxHealth = 100f;
@@ -34,5 +35,17 @@ namespace RPG.Core
                 GetComponent<ActionScheduler>().CancelCurrentAction();
             }
         }
+
+        public object CaptureState()
+        {
+            return health;
+        }
+
+        public void RestoreState(object state)
+        {
+            health = (float)state;
+            Die();
+        }
+
     }
 }
